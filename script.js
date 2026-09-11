76b67c1e53e34b82f3d3539f56db75ea1cdf9397
c20246068.paclibar-source <c20246068.paclibar@csav.edu.ph>
2026-09-11 09:24:11 +0800

    variables (let & const)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello love.");
+console.log("Hello World!");
+const Myname = "Princess Paclibar";
+console.log("name: " + Myname);
+let age = 21;
+let number = "09704945944";
+console.log("Age: " + age)
+console.log("Number: " + number);
+
+const address = "Brgy 6, Estrella, Victorias City";
+console.log("Address: " + address);
+
