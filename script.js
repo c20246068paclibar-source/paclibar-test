0c1038ff5664df7e62a11748b3374ce2e0fb24db
c20246068.paclibar-source <c20246068.paclibar@csav.edu.ph>
2026-09-11 10:14:41 +0800

    Functions

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ console.log("Number: " + number);
 const address = "Brgy 6, Estrella, Victorias City";
 console.log("Address: " + address);
 
+//functions
+function greet(name) {
+    return `good morning, ${name}`;
+}
+console.log(greet("Rene"));
+
+function mdas(num1, num2) {
+    let m = num1 * num2;
+    let d = num1 / num2;
+    let a = num1 + num2;
+    let s = num1 - num2;
+
+    return (`Values: ${num1} and ${num2}. PRODUCT: ${m}, Quotient: ${d}, Sum: ${a}, Difference: ${s}`);
+}
+console.log(mdas(5, 3));
